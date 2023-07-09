22c23e85f3217cb866792ecfc087ccf136a40237
anshuman singh <anshuman1025@gmail.com>
2023-07-09 23:20:07 +0530

    turtle crossing

---
+++ main.py
@@ -0,0 +1,42 @@
+from turtle import Turtle, Screen
+import time
+from player import Player
+from car_manager import CarManager
+from score import Score
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+score = Score()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+
+    #detect the collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            score.game_over()
+
+    #detect sucessful crossing
+    if player.is_at_finish():
+        player.go_to_start()
+        car_manager.level_up()
+        score.increase_level()
+
+
+
+screen.exitonclick()
